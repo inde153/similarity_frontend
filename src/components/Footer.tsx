f5d2e2b0fc26b8556bd3d876a929fe3a9509d64b
inde153 <inde153@naver.com>
2024-05-13 19:43:32 +0900

    Feat : footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faUser } from '@fortawesome/free-solid-svg-icons';
+// import { Link } from 'react-router-dom';
+// const logo = 'https://bucketsimilarity.s3.ap-northeast-2.amazonaws.com/Similarity-logo+(2).png';
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="py-4">
+      <div className="w-full h-96 px-5 xl:px-0 max-w-screen-lg mx-auto flex justify-between items-center bg-lime-500">안녕!</div>
+    </footer>
+  );
+};
